89f0b167b5de398a575f580c73233f37c579c4d4
amnesiaZhangSan <47342039+amnesiaZhangSan@users.noreply.github.com>
2022-08-29 09:05:04 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,13 +4,13 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "",
+    APP_ID: "wx3cff388e28a37ae7",
     // 公众号APP_SECRET
-    APP_SECRET: "",
+    APP_SECRET: "31ffdbed27e5bebfa47fe7928309aab1",
     // 模板消息id
-    TEMPLATE_ID: "",
+    TEMPLATE_ID: "1",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    USERS: [""],
+    USERS: ["okCwI6rYZz3Rl-b5nAv32N41do9o"],
      
     /**
      * 信息配置
@@ -19,9 +19,9 @@ export const config = {
     /** 天气相关 */
 
     // 所在省份
-    PROVINCE: "广东",
+    PROVINCE: "陕西",
     // 所在城市
-    CITY: "惠州",
+    CITY: "西安",
 
     /** 生日相关 */
 
